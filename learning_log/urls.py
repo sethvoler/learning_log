b5c5bf2753f5ce3196bbdce730ad1e9f6c07786d
awbb <7593907+awbb@user.noreply.gitee.com>
2021-09-17 23:45:25 +0800

    add users in the project

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls'), name='users'),
     path('', include('learning_logs.urls'), name='learning_logs')
 ]
