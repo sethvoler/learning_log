dd0b4f89127a573b198b4c2d4ff00153b34dc60d
awbb <7593907+awbb@user.noreply.gitee.com>
2021-09-18 00:21:29 +0800

    add login page

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+"""定义users的URL模式"""
+from django.urls import path
+from django.contrib.auth.views import LoginView
+from . import views
+
+urlpatterns = [
+  # 登录页面
+  path('login/', LoginView.as_view(template_name='login.html'), name='login')
+]
