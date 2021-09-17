686d05f2277fc244ea48c33c2e42df735c295620
awbb <7593907+awbb@user.noreply.gitee.com>
2021-09-17 23:17:14 +0800

    add a page which is add new entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -13,5 +13,8 @@ urlpatterns = [
   path('topics/<topic_id>/', views.topic, name='topic'),
 
   # 用于添加新主题的网页
-  path('new_topic', views.new_topic, name='new_topic')
+  path('new_topic', views.new_topic, name='new_topic'),
+
+  # 用于添加新条目的页面
+  path('new_entry/<topic_id>/', views.new_entry, name='new_entry')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 def index(request):
   """学习笔记的主页"""
@@ -35,3 +35,22 @@ def new_topic(request):
 
   context = {'form': form}
   return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+  """在特定的主题中添加新条目"""
+  topic = Topic.objects.get(id=topic_id)
+
+  if request.method != 'POST':
+    # 未提交数据：创建一个新表单
+    form = EntryForm()
+  else:
+    # POST提交的数据，对数据进行处理
+    form = EntryForm(request.POST)
+    if form.is_valid():
+      new_entry = form.save(commit=False)
+      new_entry.topic = topic
+      new_entry.save()
+      return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+
+  context = {'topic': topic, 'form': form}
+  return render(request, 'learning_logs/new_entry.html', context)
