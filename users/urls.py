097db1ad550ed93c3b708511658c94cc7ebd170d
awbb <7593907+awbb@user.noreply.gitee.com>
2021-09-18 00:34:01 +0800

    add logout page

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
   # 登录页面
-  path('login/', LoginView.as_view(template_name='login.html'), name='login')
+  path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+
+  # 注销
+  path('logout/', views.logout_view, name='logout')
 ]
