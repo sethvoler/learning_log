257e6aa7b648fcb875980d50d53778df9489f883
awbb <7593907+awbb@user.noreply.gitee.com>
2021-09-17 22:27:14 +0800

    add page which is add new topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
   """学习笔记的主页"""
@@ -17,3 +20,18 @@ def topic(request, topic_id):
   entries = topic.entry_set.order_by('-data_added')
   context = {'topic': topic, 'entries': entries}
   return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+  """添加新主题"""
+  if request.method != 'POST':
+    # 未提交数据：创建一个新表单
+    form = TopicForm()
+  else:
+    # POST提交的数据，对数据进行处理
+    form = TopicForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return HttpResponseRedirect(reverse('topics'))
+
+  context = {'form': form}
+  return render(request, 'learning_logs/new_topic.html', context)
