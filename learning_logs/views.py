0aaa11af572be2c05e49d4d93012ce47e5bc310a
awbb <7593907+awbb@user.noreply.gitee.com>
2021-09-18 22:47:38 +0800

    add 404 for edit_entry

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -23,7 +23,7 @@ def topic(request, topic_id):
   # 确认请求的主题属于当前用户
   if topic.owner != request.user:
     raise Http404
-    
+
   entries = topic.entry_set.order_by('-data_added')
   context = {'topic': topic, 'entries': entries}
   return render(request, 'learning_logs/topic.html', context)
@@ -69,6 +69,8 @@ def edit_entry(request, entry_id):
   """编辑既有条目"""
   entry = Entry.objects.get(id=entry_id)
   topic = entry.topic
+  if topic.owner != request.user:
+    raise Http404
 
   if request.method != 'POST':
     # 初次请求，使用当前条目填充表单
